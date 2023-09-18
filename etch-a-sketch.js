e29864b5a0bfed9c933eece405e1dd1f37bfaf21
Robert Chicas <robert.a.chicas@gmail.com>
2023-09-17 21:23:38 -0700

    Used JavaScript code to create 16 div elements and insert them into the main div container

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,3 +1,10 @@
 
-let container = document.querySelector("#container");
+let container = document.getElementById('container');
+
+for (let i = 0; i < 16; i++){
+    let newDiv = document.createElement('div');
+    newDiv.id = "div" + (i+1);
+    container.appendChild(newDiv);
+}
+
 
